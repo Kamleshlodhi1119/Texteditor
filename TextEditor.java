d324e319f39b5ee77066867dacf739f307e63aa6
KAMLESH LODHI <kamleshlodhi9302@gmail.com>
2024-01-24 21:19:03 +0530

    push

--- TextEditor.java
+++ TextEditor.java
@@ -1,147 +1,177 @@
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.GraphicsEnvironment;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import javax.swing.*;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.DefaultEditorKit;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
 
-public class TextEditor extends JFrame implements ActionListener {
-
+public class TextEditor extends JFrame {
     private JTextArea textArea;
-    private JComboBox<String> fontComboBox;
-    private JComboBox<Integer> sizeComboBox;
-    private JToggleButton boldButton;
-    private JToggleButton underlineButton;
+    private JFileChooser fileChooser;
 
     public TextEditor() {
-        setTitle("Text Editor");
-        setSize(600, 500);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenuItem openMenuItem = new JMenuItem("Open");
-        openMenuItem.addActionListener(this);
-        JMenuItem saveMenuItem = new JMenuItem("Save");
-        saveMenuItem.addActionListener(this);
-
-        fileMenu.add(openMenuItem);
-        fileMenu.add(saveMenuItem);
-        menuBar.add(fileMenu);
-
-        JMenu formatMenu = new JMenu("Format");
-        boldButton = new JToggleButton("Bold");
-        boldButton.addActionListener(this);
-        underlineButton = new JToggleButton("Underline");
-        underlineButton.addActionListener(this);
-        JLabel fontLabel = new JLabel("Font:");
-        fontComboBox = new JComboBox<>(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames());
-        fontComboBox.addActionListener(this);
-        JLabel sizeLabel = new JLabel("Size:");
-        Integer[] sizes = { 8, 10, 12, 14, 16, 18, 20, 24, 28, 32 };
-        sizeComboBox = new JComboBox<>(sizes);
-        sizeComboBox.addActionListener(this);
-
-        formatMenu.add(boldButton);
-        formatMenu.add(underlineButton);
-        formatMenu.addSeparator();
-        formatMenu.add(fontLabel);
-        formatMenu.add(fontComboBox);
-        formatMenu.add(sizeLabel);
-        formatMenu.add(sizeComboBox);
-        menuBar.add(formatMenu);
-
-        setJMenuBar(menuBar);
+        setTitle("Simple Text Editor");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         textArea = new JTextArea();
         JScrollPane scrollPane = new JScrollPane(textArea);
         add(scrollPane, BorderLayout.CENTER);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+
+        JMenuItem newMenuItem = new JMenuItem("New");
+        newMenuItem.addActionListener(e -> newFile());
+        fileMenu.add(newMenuItem);
+
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener(e -> openFile());
+        fileMenu.add(openMenuItem);
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.addActionListener(e -> saveFile());
+        fileMenu.add(saveMenuItem);
+
+        JMenuItem saveAsMenuItem = new JMenuItem("Save As");
+        saveAsMenuItem.addActionListener(e -> saveFileAs());
+        fileMenu.add(saveAsMenuItem);
+
+        JMenu editMenu = new JMenu("Edit");
+        menuBar.add(editMenu);
+
+        JMenuItem copyMenuItem = new JMenuItem(new DefaultEditorKit.CopyAction());
+        copyMenuItem.setText("Copy");
+        editMenu.add(copyMenuItem);
+
+        JMenuItem cutMenuItem = new JMenuItem(new DefaultEditorKit.CutAction());
+        cutMenuItem.setText("Cut");
+        editMenu.add(cutMenuItem);
+
+        JMenuItem pasteMenuItem = new JMenuItem(new DefaultEditorKit.PasteAction());
+        pasteMenuItem.setText("Paste");
+        editMenu.add(pasteMenuItem);
+
+        JMenuItem selectAllMenuItem = new JMenuItem("Select All");
+        selectAllMenuItem.addActionListener(e -> textArea.selectAll());
+        editMenu.add(selectAllMenuItem);
+
+        JMenuItem deleteMenuItem = new JMenuItem("Delete");
+        deleteMenuItem.addActionListener(e -> deleteLastChar());
+        editMenu.add(deleteMenuItem);
+
+        JMenu formatMenu = new JMenu("Format");
+        menuBar.add(formatMenu);
+
+        JMenu fontSizeMenu = new JMenu("Change Font Size");
+        formatMenu.add(fontSizeMenu);
+        populateFontSizeMenu(fontSizeMenu);
+
+        JMenu fontFamilyMenu = new JMenu("Change Font Family");
+        formatMenu.add(fontFamilyMenu);
+        populateFontFamilyMenu(fontFamilyMenu);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Open")) {
-            openFile();
-        } else if (e.getActionCommand().equals("Save")) {
-            saveFile();
-        } else if (e.getSource() == boldButton) {
-            applyStyle(StyleConstants.Bold, boldButton.isSelected());
-        } else if (e.getSource() == underlineButton) {
-            applyStyle(StyleConstants.Underline, underlineButton.isSelected());
-        } else if (e.getSource() == fontComboBox) {
-            applyFont();
-        } else if (e.getSource() == sizeComboBox) {
-            applySize();
-        }
+    private void newFile() {
+        textArea.setText("");
+        setTitle("Untitled - Simple Text Editor");
     }
 
     private void openFile() {
-        JFileChooser fileChooser = new JFileChooser();
-        int result = fileChooser.showOpenDialog(this);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            try (BufferedReader reader = new BufferedReader(new FileReader(fileChooser.getSelectedFile()))) {
-                textArea.setText("");
+        fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text files (*.txt)", "txt"));
+        int returnVal = fileChooser.showOpenDialog(this);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                StringBuilder content = new StringBuilder();
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    textArea.append(line + "\n");
+                    content.append(line).append("\n");
                 }
-            } catch (IOException ex) {
-                showError("Error reading the file");
+                textArea.setText(content.toString());
+                setTitle(file.getName() + " - Simple Text Editor");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
     private void saveFile() {
-        JFileChooser fileChooser = new JFileChooser();
-        int result = fileChooser.showSaveDialog(this);
-        if (result == JFileChooser.APPROVE_OPTION) {
-            try (FileWriter writer = new FileWriter(fileChooser.getSelectedFile())) {
-                writer.write(textArea.getText());
-            } catch (IOException ex) {
-                showError("Error saving the file");
-            }
+        if (fileChooser == null) {
+            saveFileAs();
+        } else {
+            saveToFile(fileChooser.getSelectedFile());
         }
     }
 
-    private void applyStyle(Object bold, boolean add) {
-        StyledDocument doc = (StyledDocument) textArea.getDocument();
-        int start = textArea.getSelectionStart();
-        int end = textArea.getSelectionEnd();
-        SimpleAttributeSet set = new SimpleAttributeSet();
-        set.addAttribute(bold, add);
-        doc.setCharacterAttributes(start, end - start, set, false);
+    private void saveFileAs() {
+        fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Text files (*.txt)", "txt"));
+        int returnVal = fileChooser.showSaveDialog(this);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            if (!file.getName().endsWith(".txt")) {
+                file = new File(file.getAbsolutePath() + ".txt");
+            }
+            saveToFile(file);
+        }
     }
 
-    private void applyFont() {
-        String selectedFont = (String) fontComboBox.getSelectedItem();
+    private void saveToFile(File file) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(textArea.getText());
+            setTitle(file.getName() + " - Simple Text Editor");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteLastChar() {
+        int length = textArea.getText().length();
+        if (length > 0) {
+            textArea.setText(textArea.getText().substring(0, length - 1));
+        }
+    }
+
+    private void populateFontSizeMenu(JMenu fontSizeMenu) {
+       for (int size = 8; size <= 24; size++) {
+    final int fontSize = size;  // Introduce a final variable
+    JMenuItem sizeMenuItem = new JMenuItem(String.valueOf(size));
+    sizeMenuItem.addActionListener(e -> changeFontSize(fontSize));
+    fontSizeMenu.add(sizeMenuItem);
+}
+
+    }
+
+    private void populateFontFamilyMenu(JMenu fontFamilyMenu) {
+        String[] commonFontFamilies = {"Arial", "Times New Roman", "Courier New", "Verdana", "Comic Sans MS"};
+        for (String family : commonFontFamilies) {
+            JMenuItem familyMenuItem = new JMenuItem(family);
+            familyMenuItem.addActionListener(e -> changeFontFamily(family));
+            fontFamilyMenu.add(familyMenuItem);
+        }
+    }
+
+    private void changeFontSize(int size) {
         Font currentFont = textArea.getFont();
-        Font newFont = new Font(selectedFont, currentFont.getStyle(), currentFont.getSize());
-        textArea.setFont(newFont);
+        textArea.setFont(new Font(currentFont.getFontName(), currentFont.getStyle(), size));
     }
 
-    private void applySize() {
-        int selectedSize = (Integer) sizeComboBox.getSelectedItem();
+    private void changeFontFamily(String family) {
         Font currentFont = textArea.getFont();
-        Font newFont = new Font(currentFont.getFontName(), currentFont.getStyle(), selectedSize);
-        textArea.setFont(newFont);
-    }
-
-    private void showError(String message) {
-        JOptionPane.showMessageDialog(this, message, "Error", JOptionPane.ERROR_MESSAGE);
+        textArea.setFont(new Font(family, currentFont.getStyle(), currentFont.getSize()));
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            TextEditor textEditor = new TextEditor();
-            textEditor.setVisible(true);
+            TextEditor editor = new TextEditor();
+            editor.setVisible(true);
         });
     }
 }
